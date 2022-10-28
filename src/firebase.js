e10a9744a42d7d933ee0b8d1291dd436bebfc116
mekaiser <kaiser331042@gmail.com>
2022-10-28 12:54:52 +0600

    done

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,7 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import {
-  getFirestore
-} from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBy5gnT_tqfhdYq4U7qjTuwBGG_H-zKmn4",
-  authDomain: "slack-clone-2k22.firebaseapp.com",
-  projectId: "slack-clone-2k22",
-  storageBucket: "slack-clone-2k22.appspot.com",
-  messagingSenderId: "1047198596434",
-  appId: "1:1047198596434:web:60fef2b172f4e2f5ab0e8b",
-};
+import { getFirestore } from "firebase/firestore";
+import { firebaseConfig } from "./firebase.config.js";
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
